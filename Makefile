adf6a417acac4feca8351a6f75ae23116046a464
Libertas <horizonvei@gmail.com>
2013-02-07 11:49:41 +0800

    MIT License added

--- Makefile
+++ Makefile
@@ -1,12 +1,15 @@
 
 CC=cc
-CFLAGS = -O2
+CFLAGS = -O2 -std=c99 -Wall
 objects = musica.o
 
 musica:$(objects)
 	$(CC) $(CFLAGS)   -o musica $(objects)
 
-.PHONY:clean
+
+.PHONY:clean indent
 clean:
-	rm $(objects)
-	rm musica
+	-rm $(objects)
+	-rm musica
+indent:
+	indent -npro -kr -i8 -ts8 -sob -l80 -ss -ncs -cp1 -o musica.c musica.c
--- musica.c
+++ musica.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
+#include <string.h>
+#define INPUT_LENGTH 100
 
-int
-main()
+int on_add()
 {
-		
-		return 0;
+	return 0;
+}
+
+int on_command(char *src)
+{
+	if(strcmp(src,'add')!=0) on_add();
+	return 0;
+}
+
+int main()
+{
+
+	printf("Welcome to Musica\n"
+	       "If you don't know how to use it,entry \"help\"\n");
+
+	char command[INPUT_LENGTH];
+	while (1) {
+		printf(">");
+		for (int i = 0; i < INPUT_LENGTH; i++)
+			command[i] = 0;
+		scanf("%s[^\n]", command);
+		fflush(stdin);	//May be useless
+		on_command(command);
+	}
+
+	return 0;
 }
