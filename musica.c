535559341633a7ca0d19ea80a01e2ff5fedb610c
Libertas <horizonvei@gmail.com>
2013-02-08 19:13:34 +0800

    Some functions added

--- musica.c
+++ musica.c
@@ -4,12 +4,24 @@
 
 int on_add()
 {
+	printf("on add\n");
 	return 0;
 }
 
-int on_command(char *src)
+int on_del()
 {
-	if(strcmp(src,'add')!=0) on_add();
+	printf("on del\n");
+	return 0;
+}
+
+int on_help()
+{
+	printf("Need help?\n"
+	       "help:Show this list\n"
+	       "?:Show this list\n"
+	       "add:Add a new song list\n"
+	       "del:Delete a song list\n"
+	       "exit:Get out of here\n" "quit:Get out of here\n");
 	return 0;
 }
 
@@ -19,15 +31,28 @@ int main()
 	printf("Welcome to Musica\n"
 	       "If you don't know how to use it,entry \"help\"\n");
 
-	char command[INPUT_LENGTH];
+	char order[INPUT_LENGTH];
 	while (1) {
 		printf(">");
 		for (int i = 0; i < INPUT_LENGTH; i++)
-			command[i] = 0;
-		scanf("%s[^\n]", command);
-		fflush(stdin);	//May be useless
-		on_command(command);
+			order[i] = 0;
+		scanf("%s[^\n]", order);
+
+		if (strcmp(order, "add") == 0)
+			on_add();
+
+		if (strcmp(order, "del") == 0)
+			on_del();
+
+		if (strcmp(order, "help") == 0 || strcmp(order, "?") == 0)
+			on_help();
+
+		if (strcmp(order, "exit") == 0 || strcmp(order, "quit") == 0
+		    || strcmp(order, "bye") == 0)
+			goto l_quit;
 	}
 
+      l_quit:
+	printf("Bye\n");
 	return 0;
 }
