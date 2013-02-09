c1cb7e65aba0345f58598713eef31091f5014d3f
Libertas <horizonvei@gmail.com>
2013-02-09 12:31:38 +0800

    on_add() and on_showlist() completed

--- musica.c
+++ musica.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 #include <string.h>
-#define INPUT_LENGTH 100
+#define INPUT_LENGTH 200
+#define SONGLIST_LENGTH 20
+
+char songlist[SONGLIST_LENGTH][INPUT_LENGTH];
+int songlist_counter=0;
 
 int on_add()
 {
-	printf("on add\n");
+	printf("new directory:");
+	scanf("%s^[\n]",songlist[songlist_counter]);
+	return 0;
+}
+
+int on_showlist()
+{
+	for(int i=0;i<SONGLIST_LENGTH && songlist[i][0]!=(char)0;i++) printf("%s\n",songlist[i]);
 	return 0;
 }
 
@@ -20,6 +31,7 @@ int on_help()
 	       "help ?:Show this list\n"
 	       "add:Add a new song list\n"
 	       "del delete:Delete a song list\n"
+	       "showlist:Show the songlists you have added\n"
 	       "exit quit bye:Get out of here\n");
 	return 0;
 }
@@ -45,11 +57,15 @@ int main()
 
 		else if (strcmp(order, "help") == 0 || strcmp(order, "?") == 0)
 			on_help();
+		
+		else if (strcmp(order, "showlist") == 0)
+			on_showlist();
 
 		else if (strcmp(order, "exit") == 0
 			 || strcmp(order, "quit") == 0
 			 || strcmp(order, "bye") == 0)
 			goto l_quit;
+			
 		else if (strcmp(order, "") == 0) {
 			printf("\n");
 			goto l_quit;
