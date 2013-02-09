63d49e8d909936b57374f287c3cee2e57b7fa300
Libertas <horizonvei@gmail.com>
2013-02-09 20:19:35 +0800

    on_del is completed

--- musica.c
+++ musica.c
@@ -4,35 +4,42 @@
 #define SONGLIST_LENGTH 20
 
 char songlist[SONGLIST_LENGTH][INPUT_LENGTH];
-int songlist_counter=0;
+int songlist_counter = 0;
 
 int on_add()
 {
-	printf("new directory:");
-	scanf("%s^[\n]",songlist[songlist_counter]);
+	scanf("%s^[\n]", songlist[songlist_counter]);
+	songlist_counter++;
 	return 0;
 }
 
 int on_showlist()
 {
-	for(int i=0;i<SONGLIST_LENGTH && songlist[i][0]!=(char)0;i++) printf("%s\n",songlist[i]);
+	for (int i = 0; i < SONGLIST_LENGTH && songlist[i][0] != (char)0; i++)
+		printf("%s\n", songlist[i]);
 	return 0;
 }
 
 int on_del()
 {
-	printf("on del\n");
+	if (songlist_counter != 0) {
+		songlist_counter--;
+		printf("\"%s\"%s\n", songlist[songlist_counter], " is deleted");
+		for (int i = 0; i < INPUT_LENGTH; i++)
+			songlist[songlist_counter][i] = (char)0;
+	} else
+		printf("The songlist is empty\n");
 	return 0;
 }
 
 int on_help()
 {
 	printf("Need help?\n\n"
-	       "help ?:Show this list\n"
-	       "add:Add a new song list\n"
-	       "del delete:Delete a song list\n"
-	       "showlist:Show the songlists you have added\n"
-	       "exit quit bye:Get out of here\n");
+	       "help ? :Show this list\n"
+	       "add :Add a new song list\n"
+	       "del delete :Delete a song list\n"
+	       "showlist show :Show the songlists you have added\n"
+	       "exit quit bye :Get out of here\n");
 	return 0;
 }
 
@@ -52,20 +59,21 @@ int main()
 		if (strcmp(order, "add") == 0)
 			on_add();
 
-		else if (strcmp(order, "del") == 0)
+		else if (strcmp(order, "del" || strcmp(order,"delete")) == 0)
 			on_del();
 
 		else if (strcmp(order, "help") == 0 || strcmp(order, "?") == 0)
 			on_help();
-		
-		else if (strcmp(order, "showlist") == 0)
+
+		else if (strcmp(order, "showlist") == 0
+			 || strcmp(order, "show") == 0)
 			on_showlist();
 
 		else if (strcmp(order, "exit") == 0
 			 || strcmp(order, "quit") == 0
 			 || strcmp(order, "bye") == 0)
 			goto l_quit;
-			
+
 		else if (strcmp(order, "") == 0) {
 			printf("\n");
 			goto l_quit;
